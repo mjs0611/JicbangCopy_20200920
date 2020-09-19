48da24f0d3c5a0f8933bb7b969b1881f9650251c
JunSeung Mo <mojuns@gmail.com>
2020-09-19 16:16:09 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/jicbangcopy_20200920/MainActivity.kt
@@ -0,0 +1,11 @@
+package kr.co.tjoeun.jicbangcopy_20200920
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
