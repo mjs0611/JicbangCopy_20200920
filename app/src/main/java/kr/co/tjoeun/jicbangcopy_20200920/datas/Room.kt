d775b53ca7ebe85375626d4e6598701545fe4da9
JunSeung Mo <mojuns@gmail.com>
2020-09-20 10:43:49 +0900

    하나의 방을 표현하는 xml 파일 작성

--- app/src/main/java/kr/co/tjoeun/jicbangcopy_20200920/datas/Room.kt
+++ app/src/main/java/kr/co/tjoeun/jicbangcopy_20200920/datas/Room.kt
@@ -1,4 +1,8 @@
 package kr.co.tjoeun.jicbangcopy_20200920.datas
 
-class Room(val price:Int, val address:String, val floor:Int, val description:String) {
+class Room(
+    val price:Int,
+    val address:String,
+    val floor:Int,
+    val description:String) {
 }
