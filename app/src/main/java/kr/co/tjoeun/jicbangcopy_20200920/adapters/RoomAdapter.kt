f5ab691a35443f5cc23649b33e4a9f35266c670b
JunSeung Mo <mojuns@gmail.com>
2020-09-20 11:30:50 +0900

    실제 데이터 반영 기초 작업 ( + 설명만 실 반영)

--- app/src/main/java/kr/co/tjoeun/jicbangcopy_20200920/adapters/RoomAdapter.kt
+++ app/src/main/java/kr/co/tjoeun/jicbangcopy_20200920/adapters/RoomAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import kr.co.tjoeun.jicbangcopy_20200920.R
 import kr.co.tjoeun.jicbangcopy_20200920.datas.Room
 
@@ -21,6 +22,15 @@ class RoomAdapter(
         }
 
         val row = tempRow!!
+
+        val priceTxt = row.findViewById<TextView>(R.id.priceTxt)
+        val addressAndFloorTxt = row.findViewById<TextView>(R.id.addressAndFloorTxt)
+        val descriptionTxt = row.findViewById<TextView>(R.id.descriptionTxt)
+
+
+        var roomData = mList[position]
+
+        descriptionTxt.text = roomData.description
 
         return row
     }
